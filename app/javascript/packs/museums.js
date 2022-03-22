e53fd430ea3c3c5198a5e7b560a7b70f44f97cc6
Camille Fujisaki <cae224@cornell.edu>
2022-03-22 11:15:29 +0900

    remove key

--- app/javascript/packs/museums.js
+++ app/javascript/packs/museums.js
@@ -1,11 +1,11 @@
 import mapboxgl from 'mapbox-gl';
 
 // elements
-// const paragraph = document.querySelector('p');
+const paragraph = document.querySelector('p');
 
 // display map
 const showMap = (coordinates) => {
-  mapboxgl.accessToken = 'pk.eyJ1IjoiY2FlMjI0IiwiYSI6ImNrd2x5MXZyeDA2Z2YydW8yYzh1ZWF0bDIifQ.mpwuLzHJ0OQINNb38nlgAw';
+  mapboxgl.accessToken = ;
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
@@ -21,19 +21,34 @@ const submitButton = document.querySelector('#submit');
 submitButton.addEventListener('click', (event) => {
   event.preventDefault();
   const coordinates = document.querySelector('#text').value;
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?types=poi&proximity=-74.70850,40.78375&limit=5&access_token=pk.eyJ1IjoiY2FlMjI0IiwiYSI6ImNrd2x5MXZyeDA2Z2YydW8yYzh1ZWF0bDIifQ.mpwuLzHJ0OQINNb38nlgAw`;
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?types=poi&proximity=${coordinates}&limit=5&access_token=pk.eyJ1IjoiY2FlMjI0IiwiYSI6ImNrd2x5MXZyeDA2Z2YydW8yYzh1ZWF0bDIifQ.mpwuLzHJ0OQINNb38nlgAw`;
   fetch(url)
     .then(response => response.json())
     .then((data) => {
       console.log(data);
-      // paragraph.insertAdjacentHTML('beforeend', `<span id="coordinates">[${coordinates}]</span>`);
+      data.features.forEach((feature) => {
+        const autoComp = `
+        <span>${feature.place_name}</span>`;
+        paragraph.insertAdjacentHTML("beforeend", autoComp);
+      });
       // showMap(coordinates);
     });
 });
 
-fetch(url)
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data.features);
-    console.log(data);
+const submitButton = document.querySelector('#submit');
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const coordinates = document.querySelector('#text').value;
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?types=poi&proximity=26.944,41.855&limit=5&access_token=${token}`;
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      data.features.forEach((feature) => {
+        const postalCode = feature.context[0].id
+        const museum = feature.place_name
+        console.log(museum)
+        console.log(postalCode.substr(9, postalCode.length))
+      });
+    });
 });
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,17 +2,16 @@ const { environment } = require('@rails/webpacker')
 
 module.exports = environment
 
-// config/webpack/environment.js
-const { environment } = require('@rails/webpacker')
-
 // Bootstrap 4 has a dependency over jQuery & Popper.js:
-const webpack = require('webpack')
+const webpack = require('webpack');
+
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
+    Popper: ['popper.js', 'default'],
+    'window.jQuery': 'jquery'
   })
-)
+);
 
 module.exports = environment
