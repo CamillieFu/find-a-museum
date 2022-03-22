96038b4e480b69948eaffb319b2cdb47651a77a9
Camille Fujisaki <cae224@cornell.edu>
2022-03-22 11:28:20 +0900

    remove an additional key

--- app/javascript/packs/museums.js
+++ app/javascript/packs/museums.js
@@ -1,11 +1,11 @@
 import mapboxgl from 'mapbox-gl';
-
+require("dotenv").config()
 // elements
 const paragraph = document.querySelector('p');
 
 // display map
 const showMap = (coordinates) => {
-  mapboxgl.accessToken = ;
+  mapboxgl.accessToken = token;
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
@@ -21,7 +21,7 @@ const submitButton = document.querySelector('#submit');
 submitButton.addEventListener('click', (event) => {
   event.preventDefault();
   const coordinates = document.querySelector('#text').value;
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?types=poi&proximity=${coordinates}&limit=5&access_token=pk.eyJ1IjoiY2FlMjI0IiwiYSI6ImNrd2x5MXZyeDA2Z2YydW8yYzh1ZWF0bDIifQ.mpwuLzHJ0OQINNb38nlgAw`;
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?types=poi&proximity=${coordinates}&limit=5&access_token=${process.env.MAPBOX_API_KEY}`;
   fetch(url)
     .then(response => response.json())
     .then((data) => {
