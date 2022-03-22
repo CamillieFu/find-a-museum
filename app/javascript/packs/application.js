68848c09869dd959f0f9f4e41199c63ceca93e8d
Camille Fujisaki <cae224@cornell.edu>
2022-03-22 19:48:08 +0900

    updated env file and fixed mapbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,28 +7,18 @@ console.log('hello')
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
+
 import "channels";
 import 'bootstrap';
 import 'dotenv/config';
+import { findMuseum } from "./museums";
+import { initMapbox } from '../plugins/init_mapbox';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-const submitButton = document.querySelector('#submit');
-submitButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  const coordinates = document.querySelector('#text').value;
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?types=poi&proximity=${coordinates}&limit=5&access_token=${process.env.MAPBOX_API_KEY}`;
-  fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data);
-      data.features.forEach((feature) => {
-        const postalCode = feature.context[0].id
-        const museum = feature.place_name
-        console.log(museum)
-        console.log(postalCode.substr(9, postalCode.length))
-      });
-    });
-});
+document.addEventListener("turbolinks:load", () => {
+  findMuseum();
+  initMapbox();
+})
--- app/javascript/packs/museums.js
+++ app/javascript/packs/museums.js
@@ -1,22 +1,19 @@
-import mapboxgl from 'mapbox-gl';
 require("dotenv").config()
-// elements
-const paragraph = document.querySelector('p');
-console.log('hello')
-
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 // display map
-const showMap = (coordinates) => {
-  mapboxgl.accessToken = token;
-  const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center: coordinates,
-    zoom: 12
-  });
-  new mapboxgl.Marker()
-    .setLngLat(coordinates)
-    .addTo(map);
-};
+// const showMap = (coordinates) => {
+//   mapboxgl.accessToken = token;
+//   const map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/streets-v9',
+//     center: coordinates,
+//     zoom: 12
+//   });
+//   new mapboxgl.Marker()
+//     .setLngLat(coordinates)
+//     .addTo(map);
+// };
 
 // const submitButton = document.querySelector('#submit');
 // submitButton.addEventListener('click', (event) => {
@@ -36,20 +33,26 @@ const showMap = (coordinates) => {
 //     });
 // });
 
-const submitButton = document.querySelector('#submit');
-submitButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  const coordinates = document.querySelector('#text').value;
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?types=poi&proximity=${coordinates}&limit=5&access_token=${process.env.MAPBOX_API_KEY}`;
-  fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data);
-      data.features.forEach((feature) => {
-        const postalCode = feature.context[0].id
-        const museum = feature.place_name
-        console.log(museum)
-        console.log(postalCode.substr(9, postalCode.length))
+const findMuseum = () => {
+  const mapElement = document.getElementById('map');
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+  const submitButton = document.querySelector('#submit');
+  submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const coordinates = document.querySelector('#text').value;
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?types=poi&proximity=${coordinates}&limit=5&access_token=${mapboxgl.accessToken}`;
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+        data.features.forEach((feature) => {
+          const postalCode = feature.context[0].id
+          const museum = feature.place_name
+          console.log(museum)
+          console.log(postalCode.substr(9, postalCode.length))
+        });
       });
-    });
-});
+  });
+}
+
+export { findMuseum };
